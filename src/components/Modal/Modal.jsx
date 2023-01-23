c461ef0d08d1faa3c81aac1566fbf8a331d04c4f
Богдан <105280998+Chillbeffore@users.noreply.github.com>
2023-01-23 13:58:45 +0200

    //

---
+++ src/components/Modal/Modal.jsx
@@ -0,0 +1,39 @@
+import { useEffect } from 'react';
+import PropTypes from 'prop-types';
+import css from './Modal.module.css';
+import { createPortal } from 'react-dom';
+
+const modalRoot = document.querySelector('#modal-root');
+
+export const Modal = ({ onClose, children }) => {
+  const handleKeyDown = e => {
+    if (e.code === 'Escape') {
+      onClose();
+    }
+  };
+
+  const handleBackdropClick = e => {
+    if (e.currentTarget === e.target) {
+      onClose();
+    }
+  };
+
+  useEffect(() => {
+    window.addEventListener('keydown', handleKeyDown);
+    return () => {
+      window.removeEventListener('keydown', handleKeyDown);
+    };
+  }, []); // eslint-disable-line
+
+  return createPortal(
+    <div className={css.Overlay} onClick={handleBackdropClick}>
+      <div className={css.Modal}>{children}</div>
+    </div>,
+    modalRoot
+  );
+};
+
+Modal.propTypes = {
+  onClose: PropTypes.func,
+  children: PropTypes.object,
+};
